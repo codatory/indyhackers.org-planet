bc22b17f700cf17716c007249fa61d4ae4df3786
Alex Conner <hello@codatory.com>
2013-07-06 18:17:46 -0400

    Add Rack::Cache

--- planet.rb
+++ planet.rb
@@ -1,4 +1,4 @@
-# require 'rack/cache'
+require 'rack/cache'
 require 'sinatra'
 require 'yaml'
 require 'feedzirra'
@@ -8,6 +8,10 @@ CONFIG = YAML.load(File.new('config.yml'))
 
 FEEDS = Feedzirra::Feed.fetch_and_parse(CONFIG['feeds'])
 
+use Rack::Cache,
+  :metastore => 'file:tmp/cache/meta',
+  :entitystore => 'file:tmp/cache/entity',
+  :verbose => true
 
 def post_array
   Feedzirra::Feed.update(FEEDS.values)
