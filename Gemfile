9ec6186fa330042989dacef49d8db83d73434479
Alex Conner <hello@codatory.com>
2013-07-06 18:53:25 -0400

    Add json

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source 'https://rubygems.org'
 
+ruby '2.0.0'
+
 gem 'sinatra'
 gem 'home_run'
 gem 'feedzirra'
 gem 'builder'
+gem 'rabl'
+gem 'oj'
 gem 'rack-cache'
--- planet.rb
+++ planet.rb
@@ -3,15 +3,22 @@ require 'sinatra'
 require 'yaml'
 require 'feedzirra'
 require 'builder'
+require 'rabl'
 
 CONFIG = YAML.load(File.new('config.yml'))
-
-FEEDS = Feedzirra::Feed.fetch_and_parse(CONFIG['feeds'])
+FEEDS  = Feedzirra::Feed.fetch_and_parse(CONFIG['feeds'])
 
 use Rack::Cache,
   :metastore => 'file:tmp/cache/meta',
   :entitystore => 'file:tmp/cache/entity',
   :verbose => true
+
+Rabl.configure do |config|
+  config.escape_all_output = true
+  config.include_json_root = false
+  config.enable_json_callbacks = true
+  config.include_child_root = true
+end
 
 def post_array
   Feedzirra::Feed.update(FEEDS.values)
@@ -26,4 +33,10 @@ get '/posts.rss' do
   cache_control :public, :max_age => 3600
   @posts = post_array
   builder :feed
+end
+
+get '/posts.json' do
+  cache_control :public, :max_age => 3600
+  @posts = post_array
+  rabl :feed, :format => 'json', :content_type => 'application/json'
 end
