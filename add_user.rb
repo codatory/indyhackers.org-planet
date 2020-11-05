3889b5fd563a281a40c03da86f9b649eb8bb7ced
Alex Conner <hello@codatory.com>
2020-11-05 15:18:41 -0500

    Add ergonomics to add_user script

--- add_user.rb
+++ add_user.rb
@@ -8,20 +8,28 @@ CONFIG = YAML.load_file('config.yml')
 
 url = ARGV.pop
 
+if url[0] != 'h'
+  url = "http://#{url}"
+end
+
 puts "Attempting to find RSS Feed for #{url}"
 
 feed = Feedbag.find(url).first
+raise "No feed found!" unless feed
 
 puts "Found feed at #{feed}"
 puts "Attempting to read feed"
 
-latest = Feedjira.parse(URI.open(feed).read).entries.first
+latest = Feedjira.parse(URI.open(feed).read).entries.sort_by(&:published).last
 
 puts "Here's the latest post..."
 puts "Title:   #{latest.title}"
 puts "URL:     #{latest.url}"
+puts "Date:    #{latest.published}"
 puts "--------------------------"
 puts (latest.content || latest.summary).gsub(/<\/?[a-zA-Z0-9 ]*\/?>/, '')[0..200]
+
+raise "Latest post is too old!" if latest.published < Time.now - (365*24*60*60)
 
 CONFIG['feeds'] << feed
 CONFIG['feeds'].sort!
